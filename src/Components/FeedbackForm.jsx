1c983eeec6342751b2a8bebd7d671fbae266f9f1
KruBro <techkru697@gmail.com>
2024-08-24 21:31:05 +0530

    initial commit

---
+++ src/Components/FeedbackForm.jsx
@@ -0,0 +1,103 @@
+import React, { useState } from 'react';
+import './FeedbackForm.css'; // Import CSS for styling
+
+
+const FeedbackForm = () => {
+  const[formData, setFormData] = useState({
+    name: '',
+    email: '',
+    feedback: '',
+    rating: ''
+  });
+
+  const handleChange = (event) => {
+    const {name, value} = event.target;
+    setFormData({...formData, [name]: value});
+  }
+
+  const handleSubmit = () => {
+    event.preventDefault();
+    if(!formData.name || !formData.email || !formData.feedback || !formData.rating)
+    {
+      alert("all fields are required ! ");
+      return;
+    }
+    const confirmationMessage =` 
+      Name: ${formData.name}
+      Email: ${formData.email}
+      Feedback: ${formData.feedback}
+      Rating: ${formData.rating}
+  `; 
+  const isConfirmed = window.confirm(`Please confirm your Details:\n\n${confirmationMessage}`);
+  if(isConfirmed){
+    console.log('Submitting feedback: ', formData);
+    setFormData({
+      name: '',
+      email: '',
+      feedback: '',
+      rating: ''
+    });
+    alert("Thank you for your valuable feedback! ");  
+  }  
+}
+
+  return (
+    <>
+    <nav>
+    Tell Us What You Think
+    </nav>
+      <form onSubmit={handleSubmit} className="feedback-form">
+        <h2>We'd Love to Hear From You!</h2>
+        <p>Please share your feedback with us.</p>
+        <input type="text" 
+        name="name" 
+        placeholder='Enter Your name'
+        value={formData.name}
+        onChange={handleChange}
+        />
+        <input type="email" 
+        name='email' 
+        placeholder='example@gmail.com'
+        value={formData.email}
+        onChange={handleChange}
+        />
+        <textarea name="feedback" 
+        placeholder='Enter your feedback'
+        value={formData.feedback}
+        onChange={handleChange}
+        ></textarea>
+        <div style={{display: 'flex', gap:'10px', flexDirection:'row  '}}>
+          <span>Rate Us</span>
+          <p><input type="radio" 
+          name='rating'
+          value= "1"
+          onChange={handleChange}
+          />1</p>
+          <p><input type="radio" 
+          name='rating'
+          value= "2"
+          onChange={handleChange}
+          />2</p>
+          <p><input type="radio" 
+          name='rating'
+          value= "3"
+          onChange={handleChange}
+          />3</p>
+          <p><input type="radio" 
+          name='rating'
+          value= "4"
+          onChange={handleChange}
+          />4</p>
+          <p><input type="radio" 
+          name='rating'
+          value= "5"
+          onChange={handleChange}
+          />5</p>
+        </div>
+        <button type='submit'>Submit feedback</button>
+      </form>
+    </>
+  );
+};
+
+export default FeedbackForm;
